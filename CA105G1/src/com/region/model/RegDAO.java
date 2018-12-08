ecc95a36803ff8d7aca6e33c5de720b50abeb645
coolbee333 <coolbee333@gmail.com>
2018-12-08 20:49:35 +0800

    add DAO

---
+++ CA105G1/src/com/region/model/RegDAO.java
@@ -0,0 +1,37 @@
+package com.region.model;
+
+import java.util.List;
+
+public class RegDAO implements RegDAO_interface{
+
+	@Override
+	public void insert(RegVO regVo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(RegVO regVo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(RegVO regVo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public RegVO findByPrimaryKey(Integer reg_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RegVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
